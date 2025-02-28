76924797caf65acc49e6977c58bec95c81c0af74
Anyanwu Ugochukwu <lennyyhugoh@gmail.com>
2025-02-28 23:47:45 +0100

    created customer - model, service and controller

---
+++ src/main/java/com/darc/springrestmvc/services/CustomerService.java
@@ -0,0 +1,13 @@
+package com.darc.springrestmvc.services;
+
+import com.darc.springrestmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    List<Customer> listCustomers();
+
+    Customer getCustomerById(UUID customerId);
+}
---
+++ src/main/java/com/darc/springrestmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,56 @@
+package com.darc.springrestmvc.services;
+
+import com.darc.springrestmvc.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .customerId(UUID.randomUUID())
+                .customerName("Joe")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .customerId(UUID.randomUUID())
+                .customerName("Kenny")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .customerId(UUID.randomUUID())
+                .customerName("Kim")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getCustomerId(), customer1);
+        customerMap.put(customer2.getCustomerId(), customer2);
+        customerMap.put(customer3.getCustomerId(), customer3);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID customerId) {
+        return customerMap.get(customerId);
+    }
+}
