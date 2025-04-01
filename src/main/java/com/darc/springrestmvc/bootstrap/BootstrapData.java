bd775e7fe2eb493e9423df6e1275beb6c70e7a3b
Anyanwu Ugochukwu <lennyyhugoh@gmail.com>
2025-04-01 04:42:02 +0100

    added csv data to db and all adjust tests to run well

--- src/main/java/com/darc/springrestmvc/bootstrap/BootstrapData.java
+++ src/main/java/com/darc/springrestmvc/bootstrap/BootstrapData.java
@@ -1,17 +1,25 @@
 package com.darc.springrestmvc.bootstrap;
 
 import com.darc.springrestmvc.entities.Beer;
+import com.darc.springrestmvc.model.BeerCSVRecord;
 import com.darc.springrestmvc.model.BeerStyle;
 import com.darc.springrestmvc.entities.Customer;
 import com.darc.springrestmvc.repositories.BeerRepository;
 import com.darc.springrestmvc.repositories.CustomerRepository;
+import com.darc.springrestmvc.services.BeerCsvService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -19,20 +27,54 @@ import java.util.UUID;
 public class BootstrapData implements CommandLineRunner {
     private final BeerRepository beerRepository;
     private final CustomerRepository customerRepository;
+    private final BeerCsvService beerCsvService;
 
-
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         loadBeerData();
+        loadCsvData();
         loadCustomerData();
     }
+
+    private void loadCsvData() throws FileNotFoundException {
+        if (beerRepository.count() < 10){
+            File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+            List<BeerCSVRecord> recs = beerCsvService.convertCSV(file);
+
+            recs.forEach(beerCSVRecord -> {
+                BeerStyle beerStyle = switch (beerCSVRecord.getStyle()) {
+                    case "American Pale Lager" -> BeerStyle.LAGER;
+                    case "American Pale Ale (APA)", "American Black Ale", "Belgian Dark Ale", "American Blonde Ale" ->
+                            BeerStyle.ALE;
+                    case "American IPA", "American Double / Imperial IPA", "Belgian IPA" -> BeerStyle.IPA;
+                    case "American Porter" -> BeerStyle.PORTER;
+                    case "Oatmeal Stout", "American Stout" -> BeerStyle.STOUT;
+                    case "Saison / Farmhouse Ale" -> BeerStyle.SAISON;
+                    case "Fruit / Vegetable Beer", "Winter Warmer", "Berliner Weissbier" -> BeerStyle.WHEAT;
+                    case "English Pale Ale" -> BeerStyle.PALE_ALE;
+                    default -> BeerStyle.PILSNER;
+                };
+
+                beerRepository.save(Beer.builder()
+                        .beerName(StringUtils.abbreviate(beerCSVRecord.getBeer(), 50))
+                        .beerStyle(beerStyle)
+                        .price(BigDecimal.TEN)
+                        .upc(beerCSVRecord.getRow().toString())
+                        .quantityOnHand(beerCSVRecord.getCount())
+                        .build());
+            });
+        }
+    }
+
 
     private void loadBeerData() {
 
         if (beerRepository.count() == 0) {
 
             Beer beer1 = Beer.builder()
-                    .beerName("Bud")
+                    .beerName("Budd")
                     .beerStyle(BeerStyle.PALE_ALE)
                     .upc("1239")
                     .price(new BigDecimal("14.23"))
@@ -81,7 +123,7 @@ public class BootstrapData implements CommandLineRunner {
                     .build();
 
             Customer customer2 = Customer.builder()
-                    .customerName("Kenny")
+                    .customerName("Ken")
                     .version(1)
                     .createdDate(LocalDateTime.now())
                     .modifiedDate(LocalDateTime.now())
